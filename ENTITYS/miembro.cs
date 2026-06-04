9d919fd2bde37236120b78b6480270c35455fb72
Camilo <maelhellstar@gmail.com>
2026-06-04 14:23:39 -0300

    Commit inicial: Estructura del proyecto

---
+++ ENTITYS/miembro.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.CompilerServices;
+
+public abstract class Miembro{
+
+
+    private int id;
+    private string nombre;
+    private string apellido;
+
+
+
+public int Id {get{return this.id;}set{this.id=value;}}
+public string Nombre {get{return this.nombre;}set{this.nombre=value;}}
+public string Apellido {get{return this.apellido;}set{this.apellido=value;}}
+
+
+
+public Miembro(int id, string nombre, string apellido)
+{
+this.id = id;       
+this.nombre = nombre;
+this.apellido = apellido;
+
+}
+
+public abstract MostrarInformacion();
+}
